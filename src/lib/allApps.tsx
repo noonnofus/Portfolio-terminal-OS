196d720aa4fbebf3c8ae9be3e4dee44f8f24e9f8
noonnofus <kso51@my.bcit.ca>
2026-06-01 18:25:50 +0900

    refactor: 잔여 레거시 코드 정리 및 마이그레이션 마무리

--- src/lib/allApps.tsx
+++
@@ -1,7 +0,0 @@
-import DesktopApps from "./apps";
-import ProjectsApps from "./projectsApps";
-import { Language } from "@/app/store/features/languageSlice";
-
-export default function allApps(language: Language = "ko") {
-    return [...DesktopApps(language), ...ProjectsApps(language)];
-}
